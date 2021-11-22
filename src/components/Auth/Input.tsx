2438e54a281c919e3a7680080925ae6a2553ede9
Yevhenii <yevhenii.bakhmat@nure.ua>
2021-11-22 10:35:05 +0200

    Added tailwind and started workin on auth

---
+++ src/components/Auth/Input.tsx
@@ -0,0 +1,32 @@
+import styles from './index.module.sass'
+
+export type InputProps = {
+  label: string
+  id: string
+  placeholder: string
+  type: string
+  onChange: any
+}
+
+export const Input = ({
+  id,
+  label,
+  type,
+  placeholder,
+  onChange,
+}: InputProps) => {
+  return (
+    <div className={styles['wrapper']}>
+      <label className={styles['label']} htmlFor={id}>
+        {label}
+      </label>
+      <input
+        className={styles['input']}
+        id={id}
+        onChange={(event) => onChange(event.target.value, id)}
+        type={type}
+        placeholder={placeholder}
+      />
+    </div>
+  )
+}
---
+++ src/pages/Auth/index.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react'
+import { Input } from '../../components/Auth/Input'
+import styles from './index.module.sass'
+const Auth = () => {
+  const [user, setUser] = useState({ email: '', password: '' })
+  useEffect(() => console.log(user), [user])
+  const handelUserChange = (data, id) => {
+    setUser((previousState) => {
+      const newState = { ...previousState }
+      newState[id] = data
+      return newState
+    })
+  }
+
+  return (
+    <div className={styles['wrapper']}>
+      <form className={styles['form']}>
+        <h1 className={styles['title']}>Login</h1>
+        <Input
+          id='email'
+          label='E-mail'
+          placeholder='Enter email'
+          type='email'
+          onChange={handelUserChange}
+        />
+        <Input
+          id='password'
+          label='Password'
+          placeholder='Enter password'
+          type='password'
+          onChange={handelUserChange}
+        />
+
+        <button className={styles['button']}>Log In</button>
+      </form>
+    </div>
+  )
+}
+
+export default Auth
